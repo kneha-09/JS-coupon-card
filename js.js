6b539c644ade07f015a9d78f8db3d0c5797f0ed6
kneha-09 <nehak20011209@gmail.com>
2023-05-04 18:20:34 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,8 @@
+function loadcoupon(){
+    document.getElementById("coupon").style.visibility='visible';
+    document.getElementById('main').style.opacity='1';
+}
+function closecoupon(){
+    document.getElementById("coupon").style.visibility='hidden';
+    document.getElementById('main').style.backgroundColor="#757784";
+}
